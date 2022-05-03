3ea7a1c607d1f317cb98e3d83223718172afb33a
MirkOrlando <mirko@orlando-m.it>
2022-05-03 15:03:45 +0200

    started base markup header

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,71 @@
 <template>
   <div id="app">
-    <header>Site Header</header>
+    <SiteHeader/>
     <main>Site Main</main>
     <footer>Site Footer</footer>
   </div>
 </template>
 
 <script>
+import SiteHeader from '@/components/HeaderComponent.vue'
 
 export default {
   name: 'App',
   components: {
+    SiteHeader
   }
 }
 </script>
 
 <style lang="scss">
+
+/* common */
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 
+img {
+  max-width: 100%;
+}
+
+/* utility */
+
+.h-100 {
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
+
+/* grid structure */
+
+.container {
+  max-width: 1440px;
+  margin: auto;
+}
+
+.row {
+  display: flex;
+  margin: 0 -1rem;
+}
+
+.col {
+  padding: 0 1rem;
+  margin: 0 1rem;
+}
+
+.col-40 {
+  width: 40%;
+}
+
+.col-60 {
+  width: 60%;
+}
+
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+      <div class="container">
+        <div class="row">
+          <div class="col col-40">
+            <div class="logo">
+              <img src="@/assets/img/dc-logo.png" alt="">
+            </div>
+          </div>
+          <div class="col col-60">
+            <nav>
+              <ul>
+                <li>characters</li>
+                <li>comics</li>
+                <li>movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+</template>
